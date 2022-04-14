4d820d301928a44f1ad68216a896b131dee912f0
arminveres <armin.veres@hotmail.com>
2022-04-14 15:52:49 +0200

    minor formatting update

--- scripts/gpu.sh
+++ scripts/gpu.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
 print_gpu_usage() {
-    function getGPUUsage() {
-        gpuVendor=$(glxinfo | rg "OpenGL vendor string:" | awk '{print $4}')
-        case "$gpuVendor" in
-            AMD) gpuUsage=$(cat /sys/class/drm/card0/device/gpu_busy_percent)
-            ;;
-            NVIDIA) gpuUsage=($(nvidia-smi -q -d UTILIZATION | grep Gpu | awk '{print $3}'))
-            ;;
-            # *) echo default
-            # ;;
-        esac
-    }
-    getGPUUsage
+    gpuVendor=$(glxinfo | grep "OpenGL vendor string:" | awk '{print $4}')
+    case "$gpuVendor" in
+        AMD) gpuUsage=$(cat /sys/class/drm/card0/device/gpu_busy_percent)
+        ;;
+        NVIDIA) gpuUsage=($(nvidia-smi -q -d UTILIZATION | grep Gpu | awk '{print $3}'))
+        ;;
+        Mesa/X.org) echo SSH
+        ;;
+    esac
     gpuUsage=("${gpuUsage[@]/%/%}")
 
     function join_by { local IFS="$1"; shift; echo "$*"; }
